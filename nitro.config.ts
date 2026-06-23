5891885f02020622c1cb308ba3d3beac8d7b9173
njabulo <njabulod007@gmail.com>
2026-06-23 23:29:04 +0200

    added vercel to nitro.confic.ts

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,4 +2,8 @@ import { defineNitroConfig } from 'nitro/config'
 
 export default defineNitroConfig({
   preset: 'vercel',
+  serverEntry: {
+    handler: './server.ts',
+    format: 'web',
+  },
 })
